f4ecba8902716a791f1c41b4f7785b42e4ceafa3
CarolineWallback <caroline@wallback.se>
2021-01-21 13:38:05 +0100

    Small initial changes

--- Program.cs
+++ Program.cs
@@ -7,16 +7,23 @@ namespace Rock_Paper_Scissors
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to this game of Rock, Paper, Scissors.");
-            Console.WriteLine("You will play against the computer. I think you know the rules, but if not, here they come...");
+            Console.WriteLine("You will play against the computer. I think you know the rules, but if not, here they are...");
             Console.WriteLine("You get to choose if you want to play out the rock, the paper or the scissor.");
             Console.WriteLine("The computer will play any out the objects totally random.");
-            Console.WriteLine("Rock wins over scissor, scissor wins over paper, and paper wins over rock.")
-            Console.WriteLine("Do you understand?");
+            Console.WriteLine("Rock wins over scissor, scissor wins over paper, and paper wins over rock.");
+            Console.WriteLine("Do you understand, Y/N?");
             string answer = Console.ReadLine();
 
-            if (answer == "NO" || answer == "no" || answer = "No" )
+            if (answer == "N")
             {
-                Console.WriteLine("Ok, then you should google the rules of this game and then come back");
+                Console.WriteLine("Ok, then you should google the rules of this game and then come back. Bye bye!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            else 
+            {
+                Program.RockPaperScissor();
             }
 
 
@@ -26,6 +33,9 @@ namespace Rock_Paper_Scissors
 
         static void RockPaperScissor() 
         {
+            Random RPS = new Random();
+
+
             Console.WriteLine("Choose Rock, Paper or Scissor. Type in R, P or S");
         }
 
